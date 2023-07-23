187ebc670285e3c1db56e1c17e684116bfd0b85d
Majd Orabi <orabi.majd18@gmail.com>
2023-07-23 18:52:14 +0300

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // commnead asd asd aw asdqw
     }
 
     // Update is called once per frame
