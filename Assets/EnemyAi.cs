4b37149252b07010eeb3fe9d904372b9a33d633f
abduljawada <125431874+abduljawada@users.noreply.github.com>
2023-08-05 12:16:43 +0400

    Spawning

--- Assets/EnemyAi.cs
+++ Assets/EnemyAi.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
+    [SerializeField] private GameObject mutationPrefab;
     [SerializeField] private Vector2 moveDir = new(1,0);
     [SerializeField] private float speed = 3.5f;
 
@@ -12,13 +13,19 @@ public class EnemyAi : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
-	{
-            other.gameObject.GetComponent<Health>().LoseHealth();
-            moveDir *= -1;
-	}
-	else if (other.gameObject.CompareTag("Obstacle"))
-	{
-            moveDir *= -1;
-	}
+        {
+	        other.gameObject.GetComponent<Health>().LoseHealth();
+	        moveDir *= -1;
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+	        moveDir *= -1;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (!gameObject.scene.isLoaded) return;
+        Instantiate(mutationPrefab, transform.position, Quaternion.identity);
     }
 }
