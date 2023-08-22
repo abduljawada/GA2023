d2de8c313edbbc6c22ac87072d26630fef94b508
abduljawada <125431874+abduljawada@users.noreply.github.com>
2023-08-22 16:57:59 +0400

    Reworked layers, but a bug with jumper

--- Assets/Scripts/DamageOnTrigger.cs
+++ Assets/Scripts/DamageOnTrigger.cs
@@ -4,6 +4,7 @@ public class DamageOnTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //TODO: Check if other's layer is in collision layer mask
         Debug.Log(name + " hitting " + other.name);
         other.GetComponent<Health>().LoseHealth();
     }
